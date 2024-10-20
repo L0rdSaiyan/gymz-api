600c0fe9010c2a81aec4ab35336f80795c4ed3fc
Victor <89421533+L0rdSaiyan@users.noreply.github.com>
2024-10-20 18:37:12 -0300

    Update app.py

--- gymz-api-main/app.py
+++ gymz-api-main/app.py
@@ -224,5 +224,5 @@ def get_user_exercice(id):
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(debug=True)
